7d05207d237f38029b041826d22daabc30c136a3
Yoshitaka Jingu <jingu@Jingu-iMac.local>
2013-05-27 11:27:35 +0900

    Add dependencies for BEAR.Saturday on LIVE enviroment.

---
+++ deploy/recipes/bear-saturday.rb
@@ -0,0 +1,29 @@
+#
+# Cookbook Name:: deploy
+# Recipe:: eccube
+#
+node[:deploy].each do |application, deploy|
+  if deploy[:application_type] != 'php'
+    Chef::Log.debug("Skipping deploy::php application #{application} as it is not an PHP app")
+    next
+  end
+  script "chmod_applicaton" do
+    Chef::Log.info("logs,tmp permission change ")
+    interpreter "bash"
+    cwd deploy[:deploy_to]
+    user "root"
+    code <<-EOH
+    chmod -R 775 current/logs current/tmp
+    chown -R deploy:apache current/logs current/tmp
+    EOH
+  end
+  script "rename htaccess" do
+    Chef::Log.info("rename .htaccess ")
+    interpreter "bash"
+    cwd deploy[:deploy_to]
+    user "root"
+    code <<-EOH
+    cp current/htdocs/.htaccess.live current/htdocs/.htaccess
+    EOH
+  end
+end
